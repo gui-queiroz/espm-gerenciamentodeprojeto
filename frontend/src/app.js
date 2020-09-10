27c71808bb86a2155861babffff5c1ebdb2412af
rafael <rafael@mswebmaker.com>
2020-09-10 20:58:50 -0300

    First Viable Structure

---
+++ frontend/src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
+app.use(express.json());
+app.set('views', path.join(__dirname, 'views'));
+app.use(require('./routes'));
+
+app.listen(process.env.PORT || 3000, () => console.log('Frontend ON!'));
