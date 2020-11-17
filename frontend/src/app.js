97eb10691970fa323f7f6f0c027ce40d98ee83e6
rafael <rafael@mswebmaker.com>
2020-11-17 20:29:56 -0300

    Renders

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.use(express.json());
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(require('./routes'));
 
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('Hello World'));
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/teste.html'));
+});
+
+router.get('/fazer-pedido', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/teste.html'));
+});
+
+router.get('/finalizar-pedido', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/teste.html'));
+});
+
+router.get('/financeiro', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/teste.html'));
+});
+
+router.get('/estoque', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/teste.html'));
+});
 
 module.exports = router;
