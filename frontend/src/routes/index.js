61365e2b60f652437fda489b8a23ee2bb5ee785c
rafael <rafael@mswebmaker.com>
2020-11-17 21:04:54 -0300

    Started financial page

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -4,23 +4,23 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/teste.html'));
+  // res.sendFile(path.join(__dirname, '../views/teste.html'));
 });
 
 router.get('/fazer-pedido', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/teste.html'));
+  // res.sendFile(path.join(__dirname, '../views/teste.html'));
 });
 
 router.get('/finalizar-pedido', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/teste.html'));
+  // res.sendFile(path.join(__dirname, '../views/teste.html'));
 });
 
 router.get('/financeiro', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/teste.html'));
+  res.sendFile(path.join(__dirname, '../views/financeiro.html'));
 });
 
 router.get('/estoque', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/teste.html'));
+  // res.sendFile(path.join(__dirname, '../views/teste.html'));
 });
 
 module.exports = router;
